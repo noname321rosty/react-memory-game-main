69073d0cd347a8cf1a97514b8f91c3dafe052006
rost <bilka2001@gmail.com>
2022-08-01 16:16:53 +0300

    done!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Cards from './components/Cards'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mahjong - React</h1>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
